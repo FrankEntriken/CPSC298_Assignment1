420f052f40a81a311388b59858fbfabf2e6b983c
FrankEntriken <42781627+FrankEntriken@users.noreply.github.com>
2020-01-23 12:02:24 -0800

    Add files via upload

---
+++ Problem1.cpp
@@ -0,0 +1,27 @@
+/*
+Frank Entriken
+2298368
+entriken@chapman.edu
+CPSC 298
+Problem 1
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  double weightOunces;
+  double weightMetric;
+
+  cout << "Enter the weight of a package of breakfast cereal in ounces...";
+  cin >> weightOunces;
+
+  //convert ounces to metric tons
+  weightMetric = weightOunces / 35273.92;
+  double boxes = 1 / weightMetric;
+
+  cout << "The weight of the cereal in metric tons is: " << weightMetric << endl;
+  cout << "The number of cereal boxes needed to weigh a metric ton is: " << boxes << endl;
+}
